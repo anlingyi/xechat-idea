44787c047565ab54384313c473abe31a0616fe63
anlingyi <1090172196@qq.com>
2021-12-05 16:24:26 +0800

    代码优化

--- xechat-server/src/main/java/cn/xeblog/server/service/DefaultResponseHistoryService.java
+++ xechat-server/src/main/java/cn/xeblog/server/service/DefaultResponseHistoryService.java
@@ -2,10 +2,10 @@ package cn.xeblog.server.service;
 
 import cn.xeblog.commons.entity.Response;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Collectors;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author anlingyi
@@ -13,41 +13,88 @@ import java.util.stream.Collectors;
  */
 public class DefaultResponseHistoryService extends AbstractResponseHistoryService {
 
-    private final ArrayBlockingQueue<Response> queue;
+    private final ExecutorService threadPool = new ThreadPoolExecutor(1, 2, 60,
+            TimeUnit.SECONDS, new LinkedBlockingQueue<>(500), new ThreadPoolExecutor.CallerRunsPolicy());
 
-    private final ReentrantLock lock = new ReentrantLock(true);
+    private final int size;
 
-    private final int cpuNum = Runtime.getRuntime().availableProcessors();
+    private final Response[] queue;
 
-    private final ExecutorService threadPool = new ThreadPoolExecutor(cpuNum, cpuNum * 2, 60,
-            TimeUnit.SECONDS, new ArrayBlockingQueue<>(500), new ThreadPoolExecutor.CallerRunsPolicy());
+    private final ReentrantReadWriteLock readWriteLock;
+
+    private int count;
+
+    private int takeIndex;
+
+    private int putIndex;
 
     public DefaultResponseHistoryService() {
         this(10);
     }
 
     public DefaultResponseHistoryService(int size) {
-        this.queue = new ArrayBlockingQueue<>(size, true);
+        this.size = size;
+        this.queue = new Response[size];
+        this.readWriteLock = new ReentrantReadWriteLock(true);
     }
 
     @Override
     protected void addHistoryHandler(Response response) {
-        threadPool.execute(() -> {
-            if (!queue.offer(response)) {
-                lock.lock();
-                try {
-                    queue.poll();
-                    queue.offer(response);
-                } finally {
-                    lock.unlock();
-                }
+        threadPool.execute(() -> offer(response));
+    }
+
+    private boolean offer(Response response) {
+        readWriteLock.writeLock().lock();
+        try {
+            if (count == size) {
+                poll();
             }
-        });
+
+            queue[putIndex] = response;
+            count++;
+            if (++putIndex == size) {
+                putIndex = 0;
+            }
+
+            return true;
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
+    }
+
+    private Response poll() {
+        if (count == 0) {
+            return null;
+        }
+
+        Response obj = queue[takeIndex];
+        queue[takeIndex] = null;
+        count--;
+        if (++takeIndex == size) {
+            takeIndex = 0;
+        }
+
+        return obj;
     }
 
     @Override
     public List<Response> getHistory() {
-        return queue.stream().collect(Collectors.toList());
+        List<Response> list = new ArrayList<>();
+
+        readWriteLock.readLock().lock();
+        try {
+            int start = takeIndex;
+            for (int i = 0; i < count; i++) {
+                list.add(queue[start]);
+                if (++start == size) {
+                    start = 0;
+                }
+            }
+
+            return list;
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
 }
