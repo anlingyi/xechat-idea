fb12c45b8140247275210fa19cea7e5472c22dea
anlingyi <1090172196@qq.com>
2022-06-27 07:18:58 +0800

    用户名持久化

---
+++ xechat-plugin/src/main/java/cn/xeblog/plugin/persistence/PersistenceData.java
@@ -0,0 +1,19 @@
+package cn.xeblog.plugin.persistence;
+
+import lombok.Data;
+
+/**
+ * 数据持久化
+ *
+ * @author anlingyi
+ * @date 2022/6/27 5:39 上午
+ */
+@Data
+public class PersistenceData {
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+}
---
+++ xechat-plugin/src/main/java/cn/xeblog/plugin/persistence/PersistenceService.java
@@ -0,0 +1,31 @@
+package cn.xeblog.plugin.persistence;
+
+import cn.xeblog.commons.constants.Commons;
+import cn.xeblog.plugin.cache.DataCache;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author anlingyi
+ * @date 2022/6/27 5:43 上午
+ */
+@State(name = Commons.KEY_PREFIX + "data", storages = {@Storage(Commons.KEY_PREFIX + "data.xml")})
+public class PersistenceService implements PersistentStateComponent<PersistenceData> {
+
+    private static final PersistenceData data = new PersistenceData();
+
+    @Override
+    public @Nullable PersistenceData getState() {
+        data.setUsername(DataCache.username);
+        return data;
+    }
+
+    @Override
+    public void loadState(@NotNull PersistenceData state) {
+        DataCache.username = state.getUsername();
+    }
+
+}
