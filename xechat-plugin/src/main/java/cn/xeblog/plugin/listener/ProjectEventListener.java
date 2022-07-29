f52c9dc7233b3709d43b9f90036f29b5a623d0f1
lyf <761443630@qq.com>
2022-07-29 17:30:43 +0800

    修复多IDEA窗口时，关闭插件所在窗口后找不到插件的BUG

--- xechat-plugin/src/main/java/cn/xeblog/plugin/listener/ProjectEventListener.java
+++ xechat-plugin/src/main/java/cn/xeblog/plugin/listener/ProjectEventListener.java
@@ -1,9 +1,15 @@
 package cn.xeblog.plugin.listener;
 
+import cn.hutool.core.io.IoUtil;
+import cn.xeblog.plugin.factory.MainWindowFactory;
 import cn.xeblog.plugin.ui.MainWindow;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.wm.*;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.io.InputStream;
 
 /**
  * @author anlingyi
@@ -11,9 +17,30 @@ import org.jetbrains.annotations.NotNull;
  */
 public class ProjectEventListener implements ProjectManagerListener {
 
+    private static final String WINDOW_ID = "XEChat";
+
     @Override
     public void projectOpened(@NotNull Project project) {
         MainWindow.getInstance();
     }
 
+    @Override
+    public void projectClosed(@NotNull Project project) {
+        IdeFrame[] allProjectFrames = WindowManager.getInstance().getAllProjectFrames();
+        Project otherProject = allProjectFrames[0].getProject();
+        if (otherProject != null) {
+            ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(otherProject);
+            toolWindowManager.invokeLater(() -> {
+                ToolWindow toolWindow = toolWindowManager.getToolWindow(WINDOW_ID);
+                if (toolWindow != null) {
+                    toolWindow.remove();
+                }
+                InputStream inputStream = ProjectEventListener.class.getResourceAsStream("/images/logo.png");
+                ImageIcon icon = new ImageIcon(IoUtil.readBytes(inputStream));
+                RegisterToolWindowTask xeChat = RegisterToolWindowTask.notClosable(WINDOW_ID, icon);
+                toolWindow = toolWindowManager.registerToolWindow(xeChat);
+                new MainWindowFactory().createToolWindowContent(otherProject, toolWindow);
+            });
+        }
+    }
 }
