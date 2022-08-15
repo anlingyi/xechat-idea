fd3e0490a0afcc8880f310285b1533cbc0625c7d
anlingyi <1090172196@qq.com>
2022-08-15 22:35:04 +0800

    浏览器优化调整

--- xechat-plugin/src/main/java/cn/xeblog/plugin/tools/browser/ui/BrowserUI.java
+++ xechat-plugin/src/main/java/cn/xeblog/plugin/tools/browser/ui/BrowserUI.java
@@ -2,14 +2,12 @@ package cn.xeblog.plugin.tools.browser.ui;
 
 import cn.hutool.core.util.StrUtil;
 import cn.xeblog.plugin.enums.Command;
-import com.intellij.ui.JBColor;
-import com.intellij.ui.jcef.JBCefBrowser;
-import com.intellij.ui.jcef.JBCefBrowserBase;
-import org.cef.CefClient;
-import org.cef.browser.CefBrowser;
-import org.cef.browser.CefFrame;
-import org.cef.handler.CefDisplayHandlerAdapter;
-import org.cef.handler.CefLifeSpanHandlerAdapter;
+import cn.xeblog.plugin.tools.browser.core.BrowserEventListener;
+import cn.xeblog.plugin.tools.browser.core.BrowserService;
+import cn.xeblog.plugin.tools.browser.core.JcefBrowserService;
+import cn.xeblog.plugin.tools.browser.core.UserAgent;
+import com.intellij.openapi.ui.ComboBox;
+import lombok.Getter;
 
 import javax.swing.*;
 import java.awt.*;
@@ -24,111 +22,133 @@ public class BrowserUI extends JPanel {
 
     private final static String HOME_PAGE = "https://cn.bing.com";
 
+    private BrowserService browserService;
+
     private String lastUrl;
 
-    public BrowserUI() {
-        initPanel();
+    private WindowMode windowMode;
+
+    private enum WindowMode {
+        SMALL("小", UserAgent.IPHONE, 200, 250),
+        MEDIUM("大", UserAgent.IPAD, 400, 300);
+
+        @Getter
+        String name;
+
+        @Getter
+        UserAgent userAgent;
+
+        @Getter
+        int width;
+
+        @Getter
+        int height;
+
+        WindowMode(String name, UserAgent userAgent, int width, int height) {
+            this.name = name;
+            this.userAgent = userAgent;
+            this.width = width;
+            this.height = height;
+        }
+
+        public static WindowMode getMode(String name) {
+            for (WindowMode mode : values()) {
+                if (mode.getName().equals(name)) {
+                    return mode;
+                }
+            }
+            return WindowMode.SMALL;
+        }
     }
 
-    private void initPanel() {
-        int width = 300;
-        int height = 400;
+    public BrowserUI() {
+        this.windowMode = WindowMode.SMALL;
+        init();
+    }
+
+    private void init() {
+        removeAll();
+
+        int width = this.windowMode.getWidth();
+        int height = this.windowMode.getHeight();
+
         String url = HOME_PAGE;
         if (lastUrl != null) {
             url = lastUrl;
         }
 
+        if (this.browserService != null) {
+            this.browserService.close();
+        }
+        this.browserService = new JcefBrowserService(url);
+        this.browserService.setUserAgent(windowMode.getUserAgent());
+
+        Component browserUI = browserService.getUI();
+        Dimension browserDimension = new Dimension(width, height);
+        if (windowMode == WindowMode.SMALL) {
+            browserUI.setPreferredSize(browserDimension);
+        } else {
+            browserUI.setMinimumSize(browserDimension);
+        }
+
         JTextField urlField = new JTextField(url);
-        urlField.setPreferredSize(new Dimension(200, 30));
-
-        JBCefBrowser jbCefBrowser = new JBCefBrowser(url);
-//        jbCefBrowser.setErrorPage(JBCefBrowserBase.ErrorPage.DEFAULT);
-//        jbCefBrowser.setPageBackgroundColor(colorToHex(JBColor.background()));
-        CefBrowser cefBrowser = jbCefBrowser.getCefBrowser();
-        CefClient client = cefBrowser.getClient();
-
-        client.addDisplayHandler(new CefDisplayHandlerAdapter() {
-            @Override
-            public void onAddressChange(CefBrowser browser, CefFrame frame, String url) {
-                if (!StrUtil.startWith(url, "http")) {
-                    return;
-                }
-
-                lastUrl = url;
-                urlField.setText(url);
-            }
-        });
-
-        client.removeLifeSpanHandler();
-        client.addLifeSpanHandler(new CefLifeSpanHandlerAdapter() {
-            @Override
-            public boolean onBeforePopup(CefBrowser browser, CefFrame frame, String target_url, String target_frame_name) {
-                if (StrUtil.endWithAnyIgnoreCase(target_url, "jpg", "png", "gif", "svg", "pdf", "bmp", "webp")) {
-                    return false;
-                }
-
-                cefBrowser.loadURL(target_url);
-                return true;
-            }
-
-            @Override
-            public void onBeforeClose(CefBrowser browser) {
-                SwingUtilities.invokeLater(() -> {
-                    jbCefBrowser.dispose();
-                    removeAll();
-                    initPanel();
-                    updateUI();
-                });
-            }
-        });
+        urlField.setPreferredSize(new Dimension(width * 2 / 3, 30));
 
         Dimension buttonDimension = new Dimension(50, 25);
         Box hbox = Box.createHorizontalBox();
+
+        ComboBox windowModeBox = new ComboBox();
+        windowModeBox.setPreferredSize(buttonDimension);
+        for (WindowMode mode : WindowMode.values()) {
+            windowModeBox.addItem(mode.getName());
+        }
+        windowModeBox.setSelectedItem(windowMode.getName());
+        windowModeBox.addItemListener(l -> {
+            windowMode = WindowMode.getMode(l.getItem().toString());
+            init();
+        });
+        hbox.add(windowModeBox);
+
         JButton exitButton = new JButton("✕");
         exitButton.setToolTipText("退出");
         exitButton.setPreferredSize(buttonDimension);
-        exitButton.addActionListener(l -> {
-            jbCefBrowser.dispose();
-            Command.OVER.exec();
-        });
+        exitButton.addActionListener(l -> Command.OVER.exec());
         hbox.add(exitButton);
 
         JButton homeButton = new JButton("♨");
         homeButton.setToolTipText("主页");
         homeButton.setPreferredSize(buttonDimension);
-        homeButton.addActionListener(l -> cefBrowser.loadURL(HOME_PAGE));
+        homeButton.addActionListener(l -> browserService.loadURL(HOME_PAGE));
         hbox.add(homeButton);
 
         JButton backButton = new JButton("←");
         backButton.setToolTipText("后退");
         backButton.setPreferredSize(buttonDimension);
-        backButton.addActionListener(l -> cefBrowser.goBack());
+        backButton.addActionListener(l -> browserService.goBack());
         hbox.add(backButton);
 
         JButton forwardButton = new JButton("→");
         forwardButton.setToolTipText("前进");
         forwardButton.setPreferredSize(buttonDimension);
-        forwardButton.addActionListener(l -> cefBrowser.goForward());
+        forwardButton.addActionListener(l -> browserService.goForward());
         hbox.add(forwardButton);
 
         JButton refreshButton = new JButton("⟳");
         refreshButton.setToolTipText("刷新");
         refreshButton.setPreferredSize(buttonDimension);
-        refreshButton.addActionListener(l -> cefBrowser.reload());
+        refreshButton.addActionListener(l -> browserService.reload());
         hbox.add(refreshButton);
         hbox.add(urlField);
 
         JPanel urlPanel = new JPanel();
         urlPanel.add(hbox);
 
-        JPanel htmlPanel = new JPanel();
-        htmlPanel.add(jbCefBrowser.getComponent());
-
-        setMinimumSize(new Dimension(width, height));
-        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         setLayout(new BorderLayout());
         add(urlPanel, BorderLayout.NORTH);
-        add(htmlPanel, BorderLayout.CENTER);
+        add(browserUI, BorderLayout.CENTER);
+        add(Box.createHorizontalStrut(10), BorderLayout.EAST);
+
+        updateUI();
 
         urlField.addKeyListener(new KeyAdapter() {
             @Override
@@ -138,27 +158,29 @@ public class BrowserUI extends JPanel {
                     if (!StrUtil.startWithAny(url, "https://", "http://")) {
                         url = "https://" + url;
                     }
-                    jbCefBrowser.loadURL(url);
+                    browserService.loadURL(url);
                 }
+            }
+        });
+
+        this.browserService.addEventListener(new BrowserEventListener() {
+            @Override
+            public void onAddressChange(String url) {
+                lastUrl = url;
+                urlField.setText(url);
+            }
+
+            @Override
+            public void onBeforeClose() {
+                SwingUtilities.invokeLater(() -> {
+                    init();
+                });
             }
         });
     }
 
-    private String colorToHex(Color color) {
-        return new StringBuffer()
-                .append("#")
-                .append(intToHex(color.getRed()))
-                .append(intToHex(color.getGreen()))
-                .append(intToHex(color.getBlue()))
-                .toString();
-    }
-
-    private String intToHex(int value) {
-        String hex = Integer.toHexString(value & 0xff);
-        if (hex.length() == 1) {
-            hex = "0" + hex;
-        }
-        return hex.toUpperCase();
+    public void close() {
+        this.browserService.close();
     }
 
 }
